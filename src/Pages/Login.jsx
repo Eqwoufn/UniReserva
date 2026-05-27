7aa7337ab0cd0e4d6a35fe18a4fa3681155bf7d4
Eqwoufn <kdmricsegarciachristian@gmail.com>
2026-05-26 22:46:50 -0500

    Login creado con botones y css

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,56 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css'; 
+
 export default function Login() {
+  const [codigo, setCodigo] = useState('');
+  const [password, setPassword] = useState('');
+
+  const manejarEnvio = (e) => {
+    e.preventDefault(); 
+  
+  const navigate = useNavigate();
+    
+    console.log("Intentando ingresar con el código:", codigo);
+    navigate('/dashboard');
+  };
+
   return (
-    <div>
-      <h2>Iniciar Sesión en UniReserva</h2>
-      <p>Aquí irá el formulario para ingresar con el código de alumno.</p>
+    <div className="login-container">
+      <div className="login-card">
+        <h2>UniReserva</h2>
+        <p>Ingresa con tu cuenta universitaria</p>
+
+        <form onSubmit={manejarEnvio}>
+          <div className="input-group">
+            <label htmlFor="codigo">Código de Alumno</label>
+            <input 
+              type="text" 
+              id="codigo"
+              placeholder="Tu codigo ulima"
+              value={codigo}
+              onChange={(e) => setCodigo(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Contraseña</label>
+            <input 
+              type="password" 
+              id="password"
+              placeholder="Tu contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn-ingresar">
+            Ingresar
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
